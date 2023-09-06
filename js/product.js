5b065c2f042680f087bb9c894d7d7db64f88e49e
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2023-09-06 11:00:43 +0200

    working on product detail page

---
+++ js/product.js
@@ -0,0 +1,51 @@
+//fetching data from rainydays API
+
+const jacket_url = "https://api.noroff.dev/api/v1/rainy-days";
+const products = document.querySelector(".products");
+
+try {
+  async function getJackets() {
+    //Showing Loading Indication
+    loadingIndicator();
+    //fetch function in use 
+    products.innerHTML = "";
+    const response = await fetch(jacket_url); // fetching the url
+    const results = await response.json(); // making the url readable that is in JSON format
+    const jacket = results; // we got an array of objects, putting that array in a variable
+
+    //to loop over that array
+
+    for (let i = 0; i < jacket.length; i++) {
+      console.log(jacket[i].title);
+      products.innerHTML += `
+        <div class="product">
+        <img src="${jacket[i].image}" class="product-image"/>
+        <h4 class="product_title">${jacket[i].title}</h4>
+        <p class="product_price">${jacket[i].price}</p>
+        <a href="productDetail.html?id=${jacket.id}&title=${jacket.title}" class="btn">Buy Now</a>
+      </div> `;
+    }
+  }
+
+  //Function for Loading Indicator
+  function loadingIndicator() {
+    const products = document.querySelector(".products");
+    products.innerHTML = "<li>Loading...</li>";
+  }
+
+  getJackets();
+}
+
+catch {
+  error();
+}
+
+
+
+
+
+/*for learning
+innerHTML is an HTML element property that has two uses for web developers:
+ 1) You can use it to get the internal HTML content of any HTML element as an HTML string.
+  2) You can also use it to set or change elements' innerHTML content.
+*/
---
+++ js/productDetail.js
@@ -0,0 +1,61 @@
+function getJacketIdFromQuery() {
+    const urlParams = new URLSearchParams(window.location.search);
+    console.log(urlParams.get("id"));
+    return urlParams.get("id");
+}
+
+function getJacketTitleFromQuery() {
+    const urlParams = new URLSearchParams(window.location.search);
+    console.log(urlParams.get("title"));
+    return urlParams.get("title");
+}
+
+//FUNCTION TO FETCH ITEM DETAIL USING ITEM ID AND TITLE-------------------------
+
+async function fetchJacketDetail() {
+
+    const itemId = getJacketIdFromQuery();
+    const itemTitle = getJacketTitleFromQuery();
+
+    if (!itemId) {
+        return;
+    }
+
+    const response = await fetch("https://api.noroff.dev/api/v1/jokes/${itemId}");
+    const jacketDetail = await response.json();
+    const titleContainer = document.querySelector(".jacketTitle");
+    const jacketDetailContainer = document.querySelector(".small_container");
+
+
+    jacketDetailContainer.innerHTML += `<h2>${jacketDetail.id}: ${jacketDetail.setup}<h2>
+    <p>${jacketDetail}</p>
+    
+    <div class="small_container">
+    <div class="gallery">
+      <img src="" alt="">
+    </div>
+    <div class="details">
+      <p class="jacketId">${jacketDetail.itemId}</p>
+      <h2 class="jacketTitle">${jacketDetail.itemTitle}<h2>
+          <h5 class="price">${jac}<h5>
+              <select class="sizes">
+                <option>Select Size</option>
+                <option>S</option>
+                <option>M</option>
+                <option>L</option>
+                <option>XL</option>
+              </select>
+              <input type="number" value="1">
+              <button>
+                <a href="checkOut.html" class="btn">Add To Cart</a>
+              </button>
+
+              <h3 class="description">Product Specifications</h3>
+
+    </div>
+  </div>
+`;
+
+}
+
+fetchJacketDetail();
